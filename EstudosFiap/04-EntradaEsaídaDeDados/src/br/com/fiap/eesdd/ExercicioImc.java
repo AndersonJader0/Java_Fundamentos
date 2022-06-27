6270b1b47944f71b465495074352372c94b3c50d
AndersonJader0 <andersonfilho.1907@aluno.saojudas.br>
2022-06-27 20:51:31 -0300

    Organizing the codes

---
+++ EstudosFiap/04-EntradaEsaídaDeDados/src/br/com/fiap/eesdd/ExercicioImc.java
@@ -0,0 +1,28 @@
+package br.com.fiap.eesdd;
+import java.util.Scanner;
+
+public class ExercicioImc {
+
+	public static void main(String[] args) {
+		Scanner leitura = new Scanner(System.in);
+		
+		System.out.print("Qual a altura em metros?: ");
+		double altura = leitura.nextDouble();
+		
+		System.out.print("Qual o peso em kg?: ");
+		double peso = leitura.nextDouble();
+		
+		double calculo = peso / (altura * altura);
+		
+		System.out.println("O seu IMC é = " + calculo);
+		
+		if (calculo < 18.5) {
+			System.out.println("Classificação: magreza");
+		}else if (calculo >= 18.5 && calculo <= 25) {
+			System.out.println("Classificação: normalidade");
+		}else {
+			System.out.println("Classificação: obesidade");
+		}
+	}
+		
+}
