a3d19b4b097fcc557bec624e84d7f5596e6c493d
AndersonJader0 <andersonfilho.1907@aluno.saojudas.br>
2022-06-30 16:42:27 -0300

    New class

---
+++ src/br/com/fiap/tds/Cachorro.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tds;
+
+public class Cachorro extends Animal{
+	
+	private String latido;
+	
+	public String getLatido() {
+		return latido;
+	}
+	public void setLatido(String latido) {
+		this.latido = latido;
+	}
+}
